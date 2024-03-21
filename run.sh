0bac7888f54197c8dc780edb94988d6ec51ebdda
Chlorle <161270356+Chloemil@users.noreply.github.com>
2024-03-22 06:45:09 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 
-curl_output=$(curl -H "Content-type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"eth_getBlockByNumber","params":["latest", false],"id":1}' https://RPCETHLAVA/)
+curl_output=$(curl -H "Content-type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"eth_getBlockByNumber","params":["latest", false],"id":1}' https://eth1.lava.build/lava-referer-16937d86-44bf-44ef-bdd1-25b35574dd2a/)
 
 
 echo "$curl_output" >> Update.tsx
